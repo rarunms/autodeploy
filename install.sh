3940631e6ef3b20d58db501bbc90441d37c71a76
Arun Radhakrishnan <rarunms@gmail.com>
2018-11-10 14:25:57 -0500

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 docker pull ganeshramr/ofs_smartcontract_accelerator:avenger
 
-docker run -p 8090:8090 -e BLOCKCHAIN_SERVICE_URL=http://localhost:8090/blockchain -e PORT=8090 ganeshramr/ofs_smartcontract_accelerator:avenger
+docker run -d -p 8090:8090 -e BLOCKCHAIN_SERVICE_URL=http://localhost:8090/blockchain -e PORT=8090 ganeshramr/ofs_smartcontract_accelerator:avenger
