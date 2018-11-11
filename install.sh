c2a2102c32fac1fb16a49b9309ca2fdd8199caa9
Arun Radhakrishnan <rarunms@gmail.com>
2018-11-11 07:17:08 -0500

    Delete install.sh

--- install.sh
+++
@@ -1,3 +0,0 @@
-docker pull ganeshramr/ofs_smartcontract_accelerator:avenger
-
-docker run -d -p 8090:8090 -e BLOCKCHAIN_SERVICE_URL=http://localhost:8090/blockchain -e PORT=8090 ganeshramr/ofs_smartcontract_accelerator:avenger
